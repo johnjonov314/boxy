e398482d489e374c29ca281af8a006770f3b459c
johnjonov314 <johnjonov314@gmail.com>
2025-06-17 21:54:57 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  root: '.',
-  publicDir: false, // Важно, иначе Vite будет искать public/index.html
+  root: '.',           // корневой каталог — проект
   build: {
     outDir: 'dist',
     emptyOutDir: true,
   },
-});
+  plugins: [react()],
+})
