b82c29504232f60a338a4a82f0cbee15f35cbb85
johnjonov314 <johnjonov314@gmail.com>
2025-06-18 00:45:06 +0700

    🔥 Финальная сборка: фикс index.html и структура

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   root: '.',
+  publicDir: 'public',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     assetsDir: 'assets',
     rollupOptions: {
+      input: 'public/index.html',
       output: {
         entryFileNames: 'assets/[name]-[hash].js',
         chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]',
-      },
-    },
-  },
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  }
 })
