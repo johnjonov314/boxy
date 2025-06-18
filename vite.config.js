4996e8e2998fead7809a7140ca28b2710209c6bd
johnjonov314 <johnjonov314@gmail.com>
2025-06-18 14:16:14 +0700

    фикс index.html и чистая сборка

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  root: '.',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  }
-})
+  base: './', // важно для корректной работы в Object Storage
+});
