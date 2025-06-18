7c89b2eaa8686e320f959136e3acec46d3ad1e1d
johnjonov314 <johnjonov314@gmail.com>
2025-06-18 14:10:01 +0700

    добавил .gitignore и очистил node_modules из git

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   root: '.',
-  publicDir: 'public',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: 'public/index.html',
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      }
-    }
   }
 })
